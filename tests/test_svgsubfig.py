26a9bcd0f87e4fcae26e5c5d3b0a785e182f007f
Felix Faber <felix.faber@ovgu.de>
2026-02-19 15:21:26 +0100

    Add tests

---
+++ tests/test_svgsubfig.py
@@ -0,0 +1,24 @@
+import hashlib
+import io
+import svgsubfig.utility as util
+
+from svgsubfig import SVGSubFigure
+from pathlib import Path
+
+pth_config = Path("./tests/plots.json")
+pth_svg = Path("./assets/plots.svg")
+
+fig = SVGSubFigure.from_json(pth_config)
+fig.save(pth_svg)
+
+util.convert_svg(pth_svg)
+
+
+def test_plots():
+
+    hash_expected = "23a63ed9c039f147d3d40e92934dc66ac3e24b0803ee937337bce79e2666fa38"
+
+    with pth_svg.with_suffix(".png").open(mode="rb") as f:
+        h = hashlib.file_digest(f, "sha256")
+
+        assert h.hexdigest() == hash_expected
